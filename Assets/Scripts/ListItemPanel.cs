54cb785be8f3b1af67e67586f357d150b1944ee4
Graisorn Soisakhoo <laa.kiroskirin@gmail.com>
2016-07-06 09:02:38 +0700

    Setup Project
    First setup project

---
+++ Assets/Scripts/ListItemPanel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ListItemPanel : MonoBehaviour
+{
+	public Text todoText;
+
+	public void SetText (string text)
+	{
+		this.todoText.text = text;
+	}
+}
---
+++ Assets/Scripts/ViewController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ViewController : MonoBehaviour
+{
+
+	public float itemCount;
+	public ListItemPanel todoItem;
+	public Canvas canvas;
+
+	void Start ()
+	{
+		GameObject itemObject = GameObject.FindWithTag ("ItemPanel");
+		if (itemObject != null) {
+			todoItem = itemObject.GetComponent<ListItemPanel> ();
+			GenerateItem ();
+		}
+		if (todoItem == null) {
+			Debug.Log ("Cannot find 'ListItemPanel' script");
+		}
+	}
+
+	void GenerateItem ()
+	{
+
+		for (int i = 0; i < itemCount; i++) {
+
+			Vector3 position = new Vector3 (0, 10, 0);
+
+			ListItemPanel item = Instantiate(todoItem);
+			item.transform.parent = canvas.transform;
+
+			RectTransform transform = item.GetComponent<RectTransform>();
+			item.todoText.text = "---" + i + "---";
+			transform.position = canvas.GetComponent<RectTransform>().position;
+//transform.position = position;
+//transform.offsetMin = new Vector2(0, 1);
+//transform.offsetMax = new Vector2(0, 1);
+//			transform.anchorMax = new Vector2(1, 1);
+//			transform.anchorMin = new Vector2(0,1);
+		}	
+
+	}
+	
+}
